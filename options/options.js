1bfa049def0a882576734173fbede3fb37be9477
Crowdbotics <engineering@crowdbotics.com>
2024-03-14 16:25:08 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "aline-newprdprompt-134460",
+  url: "https://aline-newprdprompt-134460.botics.co",
+  api: "https://aline-newprdprompt-134460.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
